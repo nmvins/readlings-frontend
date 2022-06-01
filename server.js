49f3a9dbc11ebe64fc7d0614bbf9ae4369b2f512
Noemi <noemi@example.com>
2022-06-01 20:34:55 +0300

    fes

--- server.js
+++ server.js
@@ -2,4 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/license'));
-app.listen(process.env.PORT || 8080); 
+app.get('/*', function(req,res) {
+res.sendFile(path.join(__dirname+
+'/dist/license/index.html'));});
+app.listen(process.env.PORT || 8080);
