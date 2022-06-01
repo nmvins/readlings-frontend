9dcd2a283c1ce4866411259b7337c77214799deb
Noemi <noemi@example.com>
2022-06-01 11:28:54 +0300

    Add env changes

--- server.js
+++ server.js
@@ -2,7 +2,4 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/license'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/license/index.html'));});
 app.listen(process.env.PORT || 8080);
